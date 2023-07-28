7f6ec51f94981cbd5f325b3eae59d5a996161ae6
FranciscoBodden <jack.dimitri1996@gmail.com>
2023-07-27 21:13:35 -0400

    [fix]: boton interactive

--- main.js
+++
@@ -1,7 +0,0 @@
-function hola(){
-    document.getElementById("objecto").innerHTML = "I have changed!";
-}
-window.onload = function what(){
-    document.getElementById("objecto").innerHTML = "I have changed!";
-};
-
